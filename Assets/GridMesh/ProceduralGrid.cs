bb400573adde7e6f7abb3c02dd1e6bd112158add
alperen <alperenerdogan0505@gmail.com>
2021-07-20 18:38:26 +0300

    grid updates

--- Assets/GridMesh/ProceduralGrid.cs
+++ Assets/GridMesh/ProceduralGrid.cs
@@ -8,7 +8,6 @@ namespace GridMesh
     {
         private Mesh _mesh;
         private Vector3[] _vertices;
-
         private int[] _triangles;
 
 //grid settings
@@ -24,6 +23,7 @@ namespace GridMesh
         private void Start()
         {
             MakeDiscreteProceduralGrid();
+            // MakeContiguousProceduralGrid();
             UpdateMesh();
         }
 
@@ -38,20 +38,67 @@ namespace GridMesh
         private void MakeDiscreteProceduralGrid()
         {
             //set array sizes
-            _vertices = new Vector3[4];
-            _triangles = new int[6];
+            _vertices = new Vector3[gridSize * gridSize * 4];
+            _triangles = new int[gridSize * gridSize * 6];
             //set vertex offsets
+            //set tracker integers
+            int v = 0;
+            int t = 0;
             float vertexOffset = cellSize * .5f;
-            //populate the vertices and triangles array
-            _vertices[0] = new Vector3(-vertexOffset, 0, -vertexOffset) + gridOffset;
-            _vertices[1] = new Vector3(-vertexOffset, 0, vertexOffset) + gridOffset;
-            _vertices[2] = new Vector3(vertexOffset, 0, -vertexOffset) + gridOffset;
-            _vertices[3] = new Vector3(vertexOffset, 0, vertexOffset) + gridOffset;
-            _triangles[0] = 0;
-            _triangles[1] = 1;
-            _triangles[2] = _triangles[3] = 2;
-            _triangles[4] = 1;
-            _triangles[5] = 3;
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    Vector3 cellOffset = new Vector3(x * cellSize, 0, y * cellSize);
+                    //populate the vertices and triangles array
+                    _vertices[v] = new Vector3(-vertexOffset, 0, -vertexOffset) + cellOffset + gridOffset;
+                    _vertices[v + 1] = new Vector3(-vertexOffset, 0, vertexOffset) + cellOffset + gridOffset;
+                    _vertices[v + 2] = new Vector3(vertexOffset, 0, -vertexOffset) + cellOffset + gridOffset;
+                    _vertices[v + 3] = new Vector3(vertexOffset, 0, vertexOffset) + cellOffset + gridOffset;
+                    _triangles[t] = v;
+                    _triangles[t + 1] = _triangles[t + 4] = v + 1;
+                    _triangles[t + 2] = _triangles[t + 3] = v + 2;
+                    _triangles[t + 5] = v + 3;
+                    v += 4;
+                    t += 6;
+                }
+            }
+        }
+        private void MakeContiguousProceduralGrid()
+        {
+            //set array sizes
+            _vertices = new Vector3[(gridSize + 1) * (gridSize+1)];
+            _triangles = new int[gridSize * gridSize * 6];
+            //set vertex offsets
+            //set tracker integers
+            int v = 0;
+            int t = 0;
+            float vertexOffset = cellSize * .5f;
+            //create vertex grid
+            for (int x = 0; x <= gridSize; x++)
+            {
+                for (int y = 0; y <= gridSize; y++)
+                {
+                    _vertices[v] = new Vector3((x * cellSize) - vertexOffset, 0, (y * cellSize) - vertexOffset);
+                    v++;
+                }
+            }
+            //reset vertex tracker
+            v = 0;
+            //setting each cell's triangles
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    _triangles[t] = v;
+                    _triangles[t + 1] = _triangles[t + 4] = v + 1;
+                    _triangles[t + 2] = _triangles[t + 3] = v + (gridSize+1);
+                    _triangles[t + 5] = v + (gridSize+1) + 1;
+                    v++;
+                    t += 6;
+                }
+                v++;
+            }
         }
     }
 }
